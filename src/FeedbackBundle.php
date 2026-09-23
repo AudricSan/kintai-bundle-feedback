c305649cfe7e3495fa91e3cec150f6eb692f8435
audricsan <audricrosier@gmail.com>
2026-09-23 14:30:32 +0900

    core: bump version to 1.1.0 line

--- src/FeedbackBundle.php
+++ src/FeedbackBundle.php
@@ -29,7 +29,7 @@ final class FeedbackBundle extends Bundle
 
     public function getVersion(): string
     {
-        return '1.0.0';
+        return '1.1.0';
     }
 
     public function getLabel(): string
